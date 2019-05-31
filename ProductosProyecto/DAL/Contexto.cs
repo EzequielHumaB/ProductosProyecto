6cec7776ec41cfe67c99183c5c5fa8228caa2580
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-05-30 20:06:47 -0400

    Agregando las propiedades de la clase Productos de la carpeta Entidades

--- ProductosProyecto/DAL/Contexto.cs
+++ ProductosProyecto/DAL/Contexto.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using ProductosProyecto.Entidades;
 
 namespace ProductosProyecto.DAL
 {
    public class Contexto
     {
-       
+       DbSet<Productos> productos { get; set; }
 
     }
 }
--- ProductosProyecto/Entidades/Productos.cs
+++ ProductosProyecto/Entidades/Productos.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ProductosProyecto.Entidades
 {
-    class Productos
+    public class Productos
     {
+        [Key]
+        public int ProductoID { get; set; }
+        public string Descripcion { get; set; }
+        public int existencia { get; set; }
+        public float costo { get; set; }
+        public float ValorInventario { get; set; }
+
+        public Productos()
+        {
+            ProductoID = 0;
+            Descripcion = string.Empty;
+            existencia = 0;
+            costo = 0.0f;
+            ValorInventario = 0.0f;
+        }
+
     }
 }
