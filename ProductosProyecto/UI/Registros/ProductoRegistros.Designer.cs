13d3e24aa3f53389ca772ac6a1277bb38d45063e
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-06-01 17:23:34 -0400

    Agregando las funciones de llenar campo y clase en el formulario de ProductoRegistro.
    En el anterior commit quise decir "Agregando el formulario agregar producto"

--- ProductosProyecto/UI/Registros/ProductoRegistros.Designer.cs
+++ ProductosProyecto/UI/Registros/ProductoRegistros.Designer.cs
@@ -28,19 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProductoRegistros));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.IDnumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.DescripciontextBox = new System.Windows.Forms.TextBox();
+            this.ExistencianumericUpDow = new System.Windows.Forms.NumericUpDown();
+            this.CostonumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.TotalInventariotextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.IDnumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ExistencianumericUpDow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CostonumericUpDown)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Producto ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Descripcion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Existencia";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Costo";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 158);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Valor Inventario";
+            // 
+            // IDnumericUpDown
+            // 
+            this.IDnumericUpDown.Location = new System.Drawing.Point(137, 27);
+            this.IDnumericUpDown.Name = "IDnumericUpDown";
+            this.IDnumericUpDown.Size = new System.Drawing.Size(50, 20);
+            this.IDnumericUpDown.TabIndex = 6;
+            // 
+            // DescripciontextBox
+            // 
+            this.DescripciontextBox.Location = new System.Drawing.Point(137, 59);
+            this.DescripciontextBox.Name = "DescripciontextBox";
+            this.DescripciontextBox.Size = new System.Drawing.Size(124, 20);
+            this.DescripciontextBox.TabIndex = 7;
+            // 
+            // ExistencianumericUpDow
+            // 
+            this.ExistencianumericUpDow.Location = new System.Drawing.Point(137, 90);
+            this.ExistencianumericUpDow.Name = "ExistencianumericUpDow";
+            this.ExistencianumericUpDow.Size = new System.Drawing.Size(50, 20);
+            this.ExistencianumericUpDow.TabIndex = 9;
+            // 
+            // CostonumericUpDown
+            // 
+            this.CostonumericUpDown.Location = new System.Drawing.Point(137, 121);
+            this.CostonumericUpDown.Name = "CostonumericUpDown";
+            this.CostonumericUpDown.Size = new System.Drawing.Size(50, 20);
+            this.CostonumericUpDown.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(37, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 57);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Nuevo";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.Location = new System.Drawing.Point(137, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 57);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Guardar";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3.Location = new System.Drawing.Point(238, 206);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 57);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Eliminar";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.Location = new System.Drawing.Point(223, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 34);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Buscar";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // TotalInventariotextBox
+            // 
+            this.TotalInventariotextBox.Location = new System.Drawing.Point(137, 158);
+            this.TotalInventariotextBox.Name = "TotalInventariotextBox";
+            this.TotalInventariotextBox.Size = new System.Drawing.Size(50, 20);
+            this.TotalInventariotextBox.TabIndex = 16;
             // 
             // ProductoRegistros
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(338, 292);
+            this.Controls.Add(this.TotalInventariotextBox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CostonumericUpDown);
+            this.Controls.Add(this.ExistencianumericUpDow);
+            this.Controls.Add(this.DescripciontextBox);
+            this.Controls.Add(this.IDnumericUpDown);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
             this.Name = "ProductoRegistros";
             this.Text = "Registros de productos";
+            ((System.ComponentModel.ISupportInitialize)(this.IDnumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ExistencianumericUpDow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CostonumericUpDown)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown IDnumericUpDown;
+        private System.Windows.Forms.TextBox DescripciontextBox;
+        private System.Windows.Forms.NumericUpDown ExistencianumericUpDow;
+        private System.Windows.Forms.NumericUpDown CostonumericUpDown;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox TotalInventariotextBox;
     }
 }
--- ProductosProyecto/UI/Registros/ProductoRegistros.cs
+++ ProductosProyecto/UI/Registros/ProductoRegistros.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ProductosProyecto.BLL;
+using ProductosProyecto.DAL;
+using ProductosProyecto.Entidades;
 
 namespace ProductosProyecto.UI.Registros
 {
@@ -17,9 +20,44 @@ namespace ProductosProyecto.UI.Registros
             InitializeComponent();
         }
 
-        private void RegistrarProductoToolStripMenuItem_Click(object sender, EventArgs e)
+       private void Limpiar()
         {
-
+            IDnumericUpDown.Value = 0;
+            DescripciontextBox.Text = string.Empty;
+            CostonumericUpDown.Value = 0;
+            ExistencianumericUpDow.Value = 0;
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private Productos llenarClase()
+        {
+            Productos productos = new Productos();
+            productos.ProductoID = (int)IDnumericUpDown.Value;
+            productos.Descripcion = DescripciontextBox.Text;
+            productos.existencia = (int)ExistencianumericUpDow.Value;
+            productos.costo = (int)CostonumericUpDown.Value;
+            int convertor = Convert.ToInt32(TotalInventariotextBox);
+            productos.ValorInventario = convertor;
+            return productos;
+        }
+
+        private void llenarCampo(Productos productos)
+        {
+            productos.ProductoID = (int)IDnumericUpDown.Value;
+            productos.Descripcion = DescripciontextBox.Text;
+            productos.costo = (int)CostonumericUpDown.Value;
+            productos.existencia = (int)ExistencianumericUpDow.Value;
+            int convertor = Convert.ToInt32(TotalInventariotextBox);
+            productos.ValorInventario = convertor;
+        }
+
+
+
+
+       
     }
 }
